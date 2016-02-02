4eb17ea480bab19534436548b70693f37bf642c6
Jakob Sack <mail@jakobsack.de>
2016-02-02 19:52:09 +0100

    Release v3.1.0-alpha4

--- lib/tilia/dav/version.rb
+++ lib/tilia/dav/version.rb
@@ -3,7 +3,7 @@ module Tilia
     # This class contains the SabreDAV version constants.
     class Version
       # Full version number
-      VERSION = '3.1.0-alpha3'
+      VERSION = '3.1.0-alpha4'
     end
   end
 end
